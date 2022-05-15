d98ca0d79619ecfbc86cef6405b1bb7faeda6f47
kefirke <kefirke@rambler.ru>
2022-05-15 20:46:13 +0300

    task 16.7_2

---
+++ cat_obj.py
@@ -0,0 +1,5 @@
+from cat import Cat
+cat1 = Cat("Барон", "мальчик", 2)
+cat2 = Cat("Сэм", "мальчик", 2)
+print(f"Кот по кличке {cat1.name}, {cat1.gender}, возраст {cat1.age} года")
+print(f"Кот по кличке {cat2.name}, {cat2.gender}, возраст {cat2.age} года")
